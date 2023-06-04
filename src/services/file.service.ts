913598ceaf1f9c5a219357674df70ef0e1c303a4
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-04 22:48:07 +0300

    Improve file service methods

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,24 +1,6 @@
 import { randomBytes } from 'crypto';
 import { constants, promises } from 'fs';
 
-const fileIsExist = async (path: string): Promise<boolean> =>  {
-  try {
-    await promises.access(path, constants.R_OK);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}
-
-const uuid = (): string => {
-  return randomBytes(16).toString('hex');
-}
-
-const generateFileName = (extension: string, prefix?: string): string => {
-  const first = prefix || uuid();
-  return `${first}_${new Date().getTime()}.${extension}`;
-}
-
 export class FileService {
   protected filesRootDir: string;
   protected savedFilesPrefix = '/files/'
@@ -27,10 +9,14 @@ export class FileService {
     this.filesRootDir = filesRootDir.slice(-1) === '/' ? filesRootDir : `${filesRootDir}/`;
   }
 
+  generateUuid(): string {
+    return randomBytes(16).toString('hex');
+  }
+
   async delete(files: string[]): Promise<void> {
     for (const file of files) {
         const preparedFile = file.replace(/\/files\//, this.filesRootDir);
-        if (await fileIsExist(preparedFile)) {
+        if (await this.isExist(preparedFile)) {
           try {
             await promises.unlink(preparedFile);
           } catch (err) {
@@ -40,8 +26,22 @@ export class FileService {
     }
   }
 
+  async isExist(path: string): Promise<boolean> {
+    try {
+      await promises.access(path, constants.R_OK);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
   async saveImage(base64: string, prefixName?: string): Promise<string> {
     return base64.startsWith(this.savedFilesPrefix) ? base64 : this.save(base64, 'images/', prefixName);
+  }
+
+  protected generateFileName = (extension: string, prefix?: string): string => {
+    const first = prefix || this.generateUuid();
+    return `${first}_${new Date().getTime()}.${extension}`;
   }
 
   protected async save(base64: string, innerDir: string, prefixName?: string): Promise<string> {
@@ -57,9 +57,9 @@ export class FileService {
     const regex = new RegExp(`^data:${fileType}\/${ext};base64,`, 'gi');
     //Extract base64 data.
     const base64Data = base64.replace(regex, '');
-    const filename = generateFileName(ext, prefixName);
+    const filename = this.generateFileName(ext, prefixName);
 
-    if (!(await fileIsExist(uploadPath))) {
+    if (!(await this.isExist(uploadPath))) {
       await promises.mkdir(uploadPath);
     }
 
