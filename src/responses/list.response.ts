04cc224e26a733a9037b2c4832d2c461dd53a8d2
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-04 19:36:23 +0300

    FIx response list data type

--- src/responses/list.response.ts
+++ src/responses/list.response.ts
@@ -1,15 +1,15 @@
 import { BaseResponse } from './base.response';
 
 export type ListResponsParams<T> = {
-  items?: T;
-  count: 0;
+  items: T[];
+  count: number;
   map?: { [id: string]: any };
 };
 
 export class ListResponse<T> extends BaseResponse<ListResponsParams<T>> {
   data: ListResponsParams<T>;
 
-  constructor(data: ListResponsParams<T>, error?: any) {
+  constructor(data?: ListResponsParams<T>, error?: any) {
     super({
       data,
       error
