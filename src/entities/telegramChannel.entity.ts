804d1507db2f9b1bf345249bfa6837772b6d2657
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-07 10:53:51 +0300

    Support telegram chat entity

---
+++ src/entities/telegramChannel.entity.ts
@@ -0,0 +1,44 @@
+import { MaxLength } from 'class-validator';
+import { BaseEntity, Column } from 'typeorm';
+
+export enum TelegramChatTypes {
+  Private = 'private',
+  Group =  'group',
+  Supergroup = 'supergroup',
+  Channel = 'channel'
+}
+
+export enum TelegramChatLimits {
+  Title = 128,
+  Description = 255,
+  Username = 32
+};
+
+export class TelegramChatEntity extends BaseEntity {
+  @Column('bigint')
+  chatId: string;
+
+  @Column('bigint')
+  ownerId: string;
+
+  @Column('bigint', { array: true, default: [] })
+  adminIds:string[];
+
+  @Column('enum', { enum: TelegramChatTypes })
+  type: string;
+
+  @Column('text', { nullable: true })
+  photo: string;
+  
+  @MaxLength(TelegramChatLimits.Title)
+  @Column('text')
+  title: string;
+
+  @MaxLength(TelegramChatLimits.Description)
+  @Column('text', { nullable: true })
+  description: string;
+
+  @MaxLength(TelegramChatLimits.Username)
+  @Column('text', { nullable: true })
+  username: string;
+}
