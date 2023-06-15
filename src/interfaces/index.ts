3df058559ad3b8b86f8f93030103ff3364254b65
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-15 15:13:33 +0300

    Add losted exports

---
+++ src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './base-entity-dto-create.interface';
+export * from './base-entity-dto-update.interface';
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
 export * from './file.service';
+export * from './repo-crud.service';
+export * from './repo-read.service';
 export * from './transaction.service';
