b4fbea4ec93e74874d6a732bee28824ab3047d4a
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-29 19:33:58 +0300

    Fix price format fn

--- src/utils/price.util.ts
+++ src/utils/price.util.ts
@@ -1,4 +1,4 @@
-export const format = (val: string, exponent = 2): number => {
+export const formatPrice = (val: string | number, exponent = 2): number => {
   const multiplier = Math.pow(10, exponent);
   return Math.round( +val * multiplier + Number.EPSILON ) / multiplier;
 }
