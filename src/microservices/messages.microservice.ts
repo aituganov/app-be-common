a08cbda830b220ade15bb89fff7d9b4f7250ccff
Vladislav Aituganov <vaaituganov@gmail.com>
2023-06-29 23:08:04 +0300

    Add messages for microservice

---
+++ src/microservices/messages.microservice.ts
@@ -0,0 +1,20 @@
+const AppMessagePrefix = { role: 'application' };
+const SessionMessagePrefix = { role: 'session' };
+const SubscriptionMessagePrefix = { role: 'subscription' };
+const TelegramMessagePrefix = { role: 'telegram' };
+
+export const AppSubscriptionActivated = { ...AppMessagePrefix, cmd: 'subscription-activated' };
+export const AppSubscriptionCanceled = { ...AppMessagePrefix, cmd: 'subscription-canceled' };
+export const AppSubscriptionExpired = { ...AppMessagePrefix, cmd: 'subscription-expired' };
+
+export const MessageSessionGet = { ...SessionMessagePrefix, cmd: 'get' };
+
+export const MessageSubscriptionGetPaymentData = { ...SubscriptionMessagePrefix, cmd: 'get-payment-data' };
+export const MessageSubscriptionCancel = { ...SubscriptionMessagePrefix, cmd: 'cancel' };
+export const MessageSubscriptionCreate = { ...SubscriptionMessagePrefix, cmd: 'create' };
+
+export const MessageTgGetBotInfo = { ...TelegramMessagePrefix, cmd: 'get-bot-info' };
+export const MessageTgChatMemberInvite = { ...TelegramMessagePrefix, cmd: 'chat-member-invite' };
+export const MessageTgChatMemberBan = { ...TelegramMessagePrefix, cmd: 'chat-member-ban' };
+export const MessageTgChatCheckAccess = { ...TelegramMessagePrefix, cmd: 'chat-check-access' };
+export const MessageTgMessageSend = { ...TelegramMessagePrefix, cmd: 'message-send' };
