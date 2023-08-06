16b706ece83ce3679124f1ffbf82813d61e6f426
Vladislav Aituganov <vaaituganov@gmail.com>
2023-08-06 22:26:20 +0300

    Add mailer ms

--- src/microservices/index.ts
+++ src/microservices/index.ts
@@ -6,6 +6,7 @@ export * from './messages.microservice';
 export enum Microservices {
   AppMain = 'APP_MAIN_MIC',
   Auth = 'AUTH_MIC',
+  Mailer = 'MAIL_MIC',
   Payment = 'PAY_MIC',
   Subscription = 'SUB_MIC',
   Telegram = 'TG_MIC'
